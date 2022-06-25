c00a0167bd061b00ff67adc2840d80d4f827b21b
Josh Brown <josh@epidev.com>
2022-06-25 16:31:32 +1000

    Parsing head,body,tail as URL-encoded b64

--- src/src/Cmd/Fuzz.hs
+++ src/src/Cmd/Fuzz.hs
@@ -2,8 +2,11 @@
 module Cmd.Fuzz
   (run) where
 
+import qualified Data.Text as T
+import           Data.Text (Text)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.UTF8 as BS.UTF8
+import qualified Data.ByteString.Base64.URL as B64
 import           Data.ByteString (ByteString)
 import Model.Args
 
@@ -20,10 +23,22 @@ data Jwt =
     }
   deriving (Show)
 
-parseJwt :: ByteString -> Either String Jwt
+parseJwt :: ByteString -> Either Text Jwt
 parseJwt s =
   case B.split (fromIntegral $ fromEnum '.') s of
-    [head,body,tail] ->
+    [head64,body64,tail64] -> do
+      head <- parseB64 head64
+      body <- parseB64 body64
+      tail <- parseB64 tail64
       Right $ Jwt head body tail
     _ ->
       Left "Supplied String not conforming to <head>.<body>.<tail>"
+
+
+parseB64 :: ByteString -> Either Text ByteString
+parseB64 s =
+  case B64.isBase64Url s of
+    False ->
+      Left "Provided ByteString is not a valid Base64 str"
+    True ->
+      B64.decodeBase64 s
